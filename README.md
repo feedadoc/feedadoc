a5a51861acf29fdd188e88f50ee51d094a7f70f4
Daniel Pickering <dan@yabble.co>
2020-03-21 14:20:31 -0700

    request directory, display basic info

--- README.md
+++ README.md
@@ -10,41 +10,49 @@ We'd love your help! Please [see the project for a prioritized list of issues (t
 
 ## Tech Stack
 
-* [Rails](https://rubyonrails.org/)
-* [React](https://reactjs.org/)
-* [GraphQL](https://graphql-ruby.org/)
-* [Apollo Client](https://www.apollographql.com/docs/react/)
-* [Material UI](https://material-ui.com/)
+- [Rails](https://rubyonrails.org/)
+- [React](https://reactjs.org/)
+- [GraphQL](https://graphql-ruby.org/)
+- [Apollo Client](https://www.apollographql.com/docs/react/)
+- [Material UI](https://material-ui.com/)
 
 ## Development
+
 The below steps assume you've forked and cloned the repo to your local machine.
 
 ### With Docker
+
 1. `docker-compose up -d`
 2. `docker-compose run web rails db:migrate db:create`
 3. Visit http://localhost:3000
 
 ### Running Ruby and Postgres locally
-1. Use `psql` to create a PG user called `feedadoc`. (We assume you have postgresql installed, on OS X you can use `brew` to install it or run [Postgres.app](https://postgresapp.com/).)
+
+1.  Use `psql` to create a PG user called `feedadoc`. (We assume you have postgresql installed, on OS X you can use `brew` to install it or run [Postgres.app](https://postgresapp.com/).)
 
         CREATE USER feedadoc WITH SUPERUSER PASSWORD 'password1';
 
-2. Install Ruby v2.7.0 (see `.ruby-version`; also, we recommend [rvm](https://rvm.io/rvm/install) to manage ruby versions.)
-3. Run `bundle`
-4. Install Yarn (On OS X: `brew install yarn`)
-5. Run `yarn`
-6. Run `rails db:create db:migrate`
-7. In one Terminal window, run `bin/webpack-dev-server`. In a second window, run `rails s`.
-8. Visit http://localhost:3000
-9. If working with emails, map localhost to `mailcatcher` in your `/etc/hosts` file.
+2.  Install Ruby v2.7.0 (see `.ruby-version`; also, we recommend [rvm](https://rvm.io/rvm/install) to manage ruby versions.)
+3.  Run `bundle`
+4.  Install Yarn (On OS X: `brew install yarn`)
+5.  Run `yarn`
+6.  Run `rails db:create db:migrate`
+7.  In one Terminal window, run `bin/webpack-dev-server`. In a second window, run `rails s`.
+8.  Visit http://localhost:3000
+9.  If working with emails, map localhost to `mailcatcher` in your `/etc/hosts` file.
 
 ## Working with Docker
+
 Running Rails with Docker is slightly more nuanced than out of the box Rails. Depending on your system, you may run into permission issues if you try to `bundle` locally while running the server on Docker. As such, it's best to only issue commands through Docker. Here are some common commands:
+
 ```bash
+# Yarn install on the webpack server
+./scripts/local yarn
+
 # Start the server
 ./scripts/local start
 
-# Stop the server 
+# Stop the server
 ./scripts/local stop
 
 # Restart the server
@@ -52,7 +60,7 @@ Running Rails with Docker is slightly more nuanced than out of the box Rails. De
 ```
 
 And to run commands you'd typically run locally, like "rails generate":
+
 ```
 ./scripts/local run "rails generate ..."
 ```
-
--- app/javascript/packs/pages/BrowseRequests.jsx
+++ app/javascript/packs/pages/BrowseRequests.jsx
@@ -1,32 +1,44 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import {makeStyles} from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 import Container from "@material-ui/core/Container";
-import {Link} from "react-router-dom";
+import Typography from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
-import { useQuery } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
 const useStyles = makeStyles(theme => ({
   heroContent: {
     backgroundColor: theme.palette.background.paper,
     marginTop: theme.spacing(3),
-    padding: theme.spacing(2),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2)
   },
   heroButtons: {
-    margin: theme.spacing(4),
-  },
+    margin: theme.spacing(4)
+  }
 }));
 
 const TOTAL_PROVIDERS = gql`
   {
     providers {
       totalCount
+      nodes {
+        id
+        city
+        firstName
+      }
     }
 
-    requests(filters: { status: new }, orderBy: { direction: ASC, sort: CITY }) {
+    requests(
+      filters: { status: new }
+      orderBy: { direction: ASC, sort: CITY }
+    ) {
       pageInfo {
         hasNextPage
         hasPreviousPage
@@ -40,9 +52,13 @@ const TOTAL_PROVIDERS = gql`
           description
           requestType
           provider {
-            id, firstName
-            city, state, neighborhood
-            role, facility
+            id
+            firstName
+            city
+            state
+            neighborhood
+            role
+            facility
           }
         }
       }
@@ -56,12 +72,60 @@ export default function BrowseRequests() {
   const { loading, error, data } = useQuery(TOTAL_PROVIDERS);
 
   return (
-    <Paper className={classes.heroContent}>
-      <Container maxWidth="sm">
-        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-          { data ? `${data.providers.totalCount} ` : ''} Care Providers in Need
-        </Typography>
-      </Container>
-    </Paper>
+    <>
+      <Paper className={classes.heroContent}>
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            {data ? `${data.providers.totalCount} ` : ""} Care Providers in Need
+          </Typography>
+        </Container>
+      </Paper>
+      <Paper>
+        <Container>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Location</TableCell>
+                <TableCell>Request type</TableCell>
+                <TableCell>Details</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data &&
+                data.requests.edges.map(
+                  ({
+                    node: {
+                      id,
+                      description,
+                      requestType,
+                      provider: { firstName, city, state }
+                    }
+                  }) => {
+                    return (
+                      <TableRow key={id}>
+                        <TableCell component="th" scope="row">
+                          {firstName}
+                        </TableCell>
+                        <TableCell>
+                          {city}, {state}
+                        </TableCell>
+                        <TableCell>{requestType}</TableCell>
+                        <TableCell>{description}</TableCell>
+                      </TableRow>
+                    );
+                  }
+                )}
+            </TableBody>
+          </Table>
+        </Container>
+      </Paper>
+    </>
   );
 }
