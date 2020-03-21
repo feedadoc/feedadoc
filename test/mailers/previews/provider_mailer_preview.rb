20f929f9dc18d756e0d087684e67bc0aef6b9e12
Tyler <tuntisz@gmail.com>
2020-03-21 00:14:52 -0600

    Center text, change font, add preview

--- test/mailers/previews/provider_mailer_preview.rb
+++ test/mailers/previews/provider_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/provider_mailer
 class ProviderMailerPreview < ActionMailer::Preview
-
+  def request_created_email
+    ProviderMailer.with(linked_token: LinkedToken.first).request_created_email
+  end
 end
