0e0b4b5a89b7781a7bda95499db2522aebcfbf9b
cheshireoctopus <chandlermoisen@gmail.com>
2020-03-22 21:56:14 -0400

    Updates providers query to handle updated_within_days filter value

--- app/graphql/filter_helpers.rb
+++ app/graphql/filter_helpers.rb
@@ -1,7 +1,6 @@
 module FilterHelpers
   def where(filters)
-    processed_filters = process_filters(filters)
-    processed_filters.with_indifferent_access
+    filters.to_h.with_indifferent_access
   end
 
   def order(order_by)
@@ -10,19 +9,5 @@ module FilterHelpers
     o = {}
     o[key] = value
     o
-  end
-
-  def process_filters(filters)
-    processed_filters = {}
-
-    filters.map do |key, value|
-      if key == :updated_within_days
-        processed_filters['updated_at'] = (DateTime.now - value)..DateTime.now
-      else
-        processed_filters[key] = value
-      end
-    end
-
-    processed_filters
   end
 end
