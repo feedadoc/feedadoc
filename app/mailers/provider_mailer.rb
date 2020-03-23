8f76f2ed3f8c6e0633f34eafe1907eddeda5eeb0
Tyler <tuntisz@gmail.com>
2020-03-22 22:01:06 -0600

    Update links sent to volunteers and providers

--- app/mailers/provider_mailer.rb
+++ app/mailers/provider_mailer.rb
@@ -2,7 +2,7 @@ class ProviderMailer < ApplicationMailer
   def request_created_email
     @provider = params[:linked_token].entity
     @request_url = HOST_AND_SCHEME + '/provider/' + @provider.to_param
-    @edit_url = HOST_AND_SCHEME + '/provider/edit?token=' + params[:linked_token].token
+    @edit_url = HOST_AND_SCHEME + '/provider/' + params[:linked_token].token + '/edit'
     mail(to: @provider.email, subject: 'Your help request has been created.')
   end
 end
