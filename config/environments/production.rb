41ea16a881bd6775abfe0a386991b1b9d04d50ba
Andrew Cantino <cantino@users.noreply.github.com>
2020-03-26 21:06:44 -0700

    Force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,9 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+  # Force SSL
+  config.force_ssl = true
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
