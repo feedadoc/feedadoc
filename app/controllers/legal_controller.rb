d73d10d1173ba16a780428a6d2068679b6760008
Tyler <tuntisz@gmail.com>
2020-03-29 00:37:45 -0600

    add controller methods

--- app/controllers/legal_controller.rb
+++ app/controllers/legal_controller.rb
@@ -1,4 +1,16 @@
 class LegalController < ApplicationController
   def index
   end
+
+  def privacy_policy
+  end
+
+  def cookie_policy
+  end
+
+  def disclaimer
+  end
+
+  def terns_of_use
+  end
 end
