e50f2db5e8f29630000d77688abf3330e6c7a593
Tyler <tuntisz@gmail.com>
2020-03-29 00:28:35 -0600

    Add footer

--- app/javascript/packs/feedadoc.jsx
+++ app/javascript/packs/feedadoc.jsx
@@ -18,6 +18,7 @@ import Header from "./components/Header";
 import ScrollToTopOnMount from "./components/ScrollToTopOnMount";
 import theme from "./theme";
 import Home from "./pages/home/Home";
+import Footer from "./components/Footer";
 
 const useStyles = makeStyles((theme) => ({
   layout: {
@@ -103,6 +104,7 @@ function App() {
               </Switch>
             </div>
           </main>
+          <Footer />
         </Router>
       </ApolloProvider>
     </MuiThemeProvider>
