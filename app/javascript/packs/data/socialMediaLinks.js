3371f2f09e88da4e2e23b0763296833f5dc0716b
Andrew Cantino <cantino@users.noreply.github.com>
2020-03-28 20:57:24 -0700

    Fix links

--- app/javascript/packs/data/socialMediaLinks.js
+++ app/javascript/packs/data/socialMediaLinks.js
@@ -1,11 +1,11 @@
 export const FACEBOOK_LINK =
-  "https://www.facebook.com/sharer/sharer.php?u=https%3A//http://hospitalhero.care/";
+  "https://www.facebook.com/sharer/sharer.php?u=https%3A//hospitalhero.care/";
 
 export const TWITTER_LINK =
-  "https://twitter.com/intent/tweet?text=Check%20out%20%40HospitalHero,%20a%20tool%20that%20matches%20hospital%20workers%20fighting%20%23COVID19%20with%20local%20volunteers%20who%20want%20to%20help.%20Please%20share%20with%20doctors,%20nurses,%20and%20all%20other%20hospital%20workers%20who%20would%20appreciate%20some%20extra%20support!%20https%3A//http://hospitalhero.care/";
+  "https://twitter.com/intent/tweet?text=Check%20out%20%40HospitalHero,%20a%20tool%20that%20matches%20hospital%20workers%20fighting%20%23COVID19%20with%20local%20volunteers%20who%20want%20to%20help.%20Please%20share%20with%20doctors,%20nurses,%20and%20all%20other%20hospital%20workers%20who%20would%20appreciate%20some%20extra%20support!%20https%3A//hospitalhero.care/";
 
 export const LINKEDIN_LINK =
-  "https://www.linkedin.com/shareArticle?mini=true&url=https%3A//http://hospitalhero.care/&title=Feed%20a%20Doc%3A%20Care%20providers%20need%20our%20help%20to%20fight%20COVID-19&summary=Check%20out%20FeedaDoc,%20a%20tool%20that%20matches%20hospital%20workers%20fighting%20%23COVID19%20with%20local%20volunteers%20who%20want%20to%20help.%20Please%20share%20with%20doctors,%20nurses,%20and%20all%20other%20hospital%20workers%20who%20would%20appreciate%20some%20extra%20support!%20https%3A//http://hospitalhero.care/&source=";
+  "https://www.linkedin.com/shareArticle?mini=true&url=https%3A//hospitalhero.care/&title=Feed%20a%20Doc%3A%20Care%20providers%20need%20our%20help%20to%20fight%20COVID-19&summary=Check%20out%20FeedaDoc,%20a%20tool%20that%20matches%20hospital%20workers%20fighting%20%23COVID19%20with%20local%20volunteers%20who%20want%20to%20help.%20Please%20share%20with%20doctors,%20nurses,%20and%20all%20other%20hospital%20workers%20who%20would%20appreciate%20some%20extra%20support!%20https%3A//hospitalhero.care/&source=";
 
 export const EMAIL_LINK =
-  "mailto:?subject=Check%20out%20%22Feed%20a%20Doc%22&body=Hi,%20I%20thought%20you%20might%20be%20interested%20in%20this%20tool%20built%20by%20volunteers%20to%20support%20care%20providers%20fighting%20COVID-19.%20It%20lets%20healthcare%20workers%20post%20a%20request%20for%20support%20and%20matches%20them%20with%20volunteers%20(either%20friends%20or%20people%20in%20their%20community).%20%0A%20https%3A//http://hospitalhero.care/";
+  "mailto:?subject=Check%20out%20%22Feed%20a%20Doc%22&body=Hi,%20I%20thought%20you%20might%20be%20interested%20in%20this%20tool%20built%20by%20volunteers%20to%20support%20care%20providers%20fighting%20COVID-19.%20It%20lets%20healthcare%20workers%20post%20a%20request%20for%20support%20and%20matches%20them%20with%20volunteers%20(either%20friends%20or%20people%20in%20their%20community).%20%0A%20https%3A//hospitalhero.care/";
