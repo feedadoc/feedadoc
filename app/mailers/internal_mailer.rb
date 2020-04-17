40e68d219856470c34a13c3674e087892560b368
James Tran <tmnhat2001@gmail.com>
2020-04-16 20:39:26 -0400

    Remove from address for internal email

--- app/mailers/internal_mailer.rb
+++ app/mailers/internal_mailer.rb
@@ -11,7 +11,7 @@ class InternalMailer < ApplicationMailer
     @provider = params[:provider]
     @volunteer = params[:volunteer]
     @response = params[:response]
-    mail(template_path: "provider_mailer", to: INTERNAL_EMAIL, from: @volunteer.email, reply_to: @volunteer.email,
+    mail(template_path: "provider_mailer", to: INTERNAL_EMAIL, reply_to: @volunteer.email,
       subject: "Offer made to #{@provider.first_name} from #{@volunteer.first_name} (RequestId: #{@provider.id})")
   end
 end
