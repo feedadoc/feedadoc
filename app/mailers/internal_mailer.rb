65bdf2e5eaec0bafea310f063f5f07b980150b8d
Tyler <tuntisz@gmail.com>
2020-04-18 14:08:28 -0600

    update nav link

--- app/mailers/internal_mailer.rb
+++ app/mailers/internal_mailer.rb
@@ -4,7 +4,7 @@ class InternalMailer < ApplicationMailer
     @provider = params[:linked_token].entity
     @request_url = HOST_AND_SCHEME + '/providers/' + @provider.to_param
     @edit_url = HOST_AND_SCHEME + '/providers/' + params[:linked_token].token + '/edit'
-    mail(template_path: 'provider_mailer', to: INTERNAL_EMAIL, reply_to: @provider.email, subject: "Request from #{@provider.full_name} (Request ID: #{@provider.id})")
+    mail(template_path: 'provider_mailer', from: INTERNAL_EMAIL, reply_to: @provider.email, subject: "Request from #{@provider.full_name} (Request ID: #{@provider.id})")
   end
 
   def volunteer_response_email
